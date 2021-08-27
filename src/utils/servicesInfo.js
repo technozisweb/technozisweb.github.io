900c81aea0c6d0e4e92f2556d308f7aad487b8cd
Richa <richatechnozisx@gmail.com>
2021-08-27 23:05:40 +0530

    services static text

---
+++ src/utils/servicesInfo.js
@@ -0,0 +1,58 @@
+const services = [
+	{
+		'title': 'Custom Software Development',
+		'description': 'We help you build a successful digital product right from ideation to the improvement, covering all aspects of modern software development to meet your needs.',
+		'points': [
+			'Our technical skillset and years of experience help us create scalable, reliable, and secure SaaS development.',
+			'We focus on serving a wide range of industry segments including retail & FMCG, logistic, healthcare, education, travel, manufacturing, and many more.',
+			'We help you build a successful digital product right from ideation to the improvement, covering all aspects of modern software development to meet your needs.'
+		]
+	},
+	{
+		'title': 'Hire Developer',
+		'description': 'We help you build a successful digital product right from ideation to the improvement, covering all aspects of modern software development to meet your needs.',
+		'points': [
+			'Our technical skillset and years of experience help us create scalable, reliable, and secure SaaS development.',
+			'We focus on serving a wide range of industry segments including retail & FMCG, logistic, healthcare, education, travel, manufacturing, and many more.',
+			'We help you build a successful digital product right from ideation to the improvement, covering all aspects of modern software development to meet your needs.'
+		]
+	},
+	{
+		'title': 'Technologies Consultant',
+		'description': 'We help you build a successful digital product right from ideation to the improvement, covering all aspects of modern software development to meet your needs.',
+		'points': [
+			'Our technical skillset and years of experience help us create scalable, reliable, and secure SaaS development.',
+			'We focus on serving a wide range of industry segments including retail & FMCG, logistic, healthcare, education, travel, manufacturing, and many more.',
+			'We help you build a successful digital product right from ideation to the improvement, covering all aspects of modern software development to meet your needs.'
+		]
+	},
+	{
+		'title': 'SAP',
+		'description': 'We help you build a successful digital product right from ideation to the improvement, covering all aspects of modern software development to meet your needs.',
+		'points': [
+			'Our technical skillset and years of experience help us create scalable, reliable, and secure SaaS development.',
+			'We focus on serving a wide range of industry segments including retail & FMCG, logistic, healthcare, education, travel, manufacturing, and many more.',
+			'We help you build a successful digital product right from ideation to the improvement, covering all aspects of modern software development to meet your needs.'
+		]
+	},
+	{
+		'title': 'Product Engineering',
+		'description': 'We help you build a successful digital product right from ideation to the improvement, covering all aspects of modern software development to meet your needs.',
+		'points': [
+			'Our technical skillset and years of experience help us create scalable, reliable, and secure SaaS development.',
+			'We focus on serving a wide range of industry segments including retail & FMCG, logistic, healthcare, education, travel, manufacturing, and many more.',
+			'We help you build a successful digital product right from ideation to the improvement, covering all aspects of modern software development to meet your needs.'
+		]
+	},
+	{
+		'title': 'Management and Support',
+		'description': 'We help you build a successful digital product right from ideation to the improvement, covering all aspects of modern software development to meet your needs.',
+		'points': [
+			'Our technical skillset and years of experience help us create scalable, reliable, and secure SaaS development.',
+			'We focus on serving a wide range of industry segments including retail & FMCG, logistic, healthcare, education, travel, manufacturing, and many more.',
+			'We help you build a successful digital product right from ideation to the improvement, covering all aspects of modern software development to meet your needs.'
+		]
+	},
+]
+
+export default services;
