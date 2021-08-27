55427789abd255667e3e06a381be36206c2b44b1
Richa <richatechnozisx@gmail.com>
2021-08-27 23:03:34 +0530

    static text files

---
+++ src/utils/technologies.js
@@ -0,0 +1,30 @@
+const technologies = [
+  {
+    title: 'FRONT',
+    path: [
+      '/static/technologies/netdevelopment.png'
+    ]
+  },
+  {
+    title: 'BACK',
+    path: [
+      '/static/technologies/backend/C#.png',
+      '/static/technologies/backend/C++.png',
+      '/static/technologies/backend/netcore.png',
+      '/static/technologies/backend/netdevelopment.png',
+      '/static/technologies/backend/php.png',
+    ]
+  },
+  {
+    title: 'DATABASE',
+    path: [
+      '/static/technologies/backend/DynamoDB.png',
+      '/static/technologies/backend/mongodb.png',
+      '/static/technologies/backend/mysql-logo.png',
+      '/static/technologies/backend/PostgreSQL.png',
+      '/static/technologies/backend/sqlserver.png',
+    ]
+  },
+]
+
+export default technologies;
