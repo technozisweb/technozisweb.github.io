1396ba6bc4d5f90a2261b2252e55f174e1fff5a8
Richa <richatechnozisx@gmail.com>
2021-09-07 12:36:16 +0530

    main footer changes

--- src/layouts/main/MainFooter.js
+++ src/layouts/main/MainFooter.js
@@ -13,6 +13,7 @@ import { Grid, Link, Divider, Container, Typography, IconButton, Stack, Popover 
 import { PATH_PAGE } from '../../routes/paths';
 //
 import Logo from '../../components/Logo';
+import Label from 'src/components/Label';
 
 // ----------------------------------------------------------------------
 
@@ -25,25 +26,18 @@ const SOCIALS = [
 
 const LINKS = [
   {
-    headline: 'Technozisz',
+    headline: 'Technozis',
     children: [
       { name: 'About us', href: PATH_PAGE.about },
       { name: 'Contact us', href: PATH_PAGE.contact },
-      { name: 'FAQs', href: PATH_PAGE.faqs }
-    ]
-  },
-  {
-    headline: 'Legal',
-    children: [
-      { name: 'Terms and Condition', href: '#' },
-      { name: 'Privacy Policy', href: '#' }
+      // { name: 'FAQs', href: PATH_PAGE.faqs }
     ]
   },
   {
     headline: 'Contact',
     children: [
-      { name: 'support@minimals.cc', href: '#' },
-      { name: 'Los Angeles, 359  Hidden Valley Road', href: '#' }
+      { name: 'contact@technozis.com', href: '#' },
+      { name: '210, Tower 2, DLF Corporate Greens, Sector 74A, Gurugram', href: '#' }
     ]
   }
 ];
@@ -57,9 +51,11 @@ const RootStyle = styled('div')(({ theme }) => ({
 
 export default function MainFooter() {
   const [anchorEl, setAnchorEl] = useState(null);
+  const [title, setTitle] = useState();
 
-  const handleClick = (event) => {
+  const handleClick = (event, name) => {
     setAnchorEl(event.currentTarget);
+    setTitle(name);
   };
 
   const handleClose = () => {
@@ -70,94 +66,104 @@ export default function MainFooter() {
   const id = open ? 'simple-popover' : undefined;
 
   return (
-    <RootStyle>
-      <Divider />
-      <Container maxWidth="lg" sx={{ pt: 10 }}>
-        <Grid
-          container
-          justifyContent={{ xs: 'center', md: 'space-between' }}
-          sx={{ textAlign: { xs: 'center', md: 'left' } }}
-        >
-          <Grid item xs={12} sx={{ mb: 3 }}>
-            <ScrollLink to="move_top" spy smooth>
-              <Logo sx={{ mx: { xs: 'auto', md: 'inherit' } }} />
-            </ScrollLink>
-          </Grid>
-          <Grid item xs={8} md={3}>
-            <Typography variant="body2" sx={{ pr: { md: 5 } }}>
+    <>
+      <RootStyle>
+        <Divider />
+        <Container maxWidth="lg" sx={{ pt: 10 }}>
+          <Grid
+            container
+            justifyContent={{ xs: 'center', md: 'space-between' }}
+            sx={{ textAlign: { xs: 'center', md: 'left' } }}
+          >
+            <Grid item xs={12} sx={{ mb: 1 }}>
+              <ScrollLink to="move_top" spy smooth>
+                <Logo sx={{ mx: { xs: 'auto', md: 'inherit' } }} />
+                {/* <Label></Label> */}
+              </ScrollLink>
+              <Label color="info">
+                Technozis
+              </Label>
+            </Grid>
+            <Grid item xs={8} md={3}>
+              {/* <Typography variant="body2" sx={{ pr: { md: 5 } }}>
               The starting point for your next project with Minimal UI Kit, built on the newest version of Material-UI
               ©, ready to be customized to your style.
-            </Typography>
+            </Typography> */}
 
-            <Stack
-              spacing={1.5}
-              direction="row"
-              justifyContent={{ xs: 'center', md: 'flex-start' }}
-              sx={{ mt: 5, mb: { xs: 5, md: 0 } }}
-            >
-              {SOCIALS.map((social) => (
-                <>
-                  <IconButton key={social.name} color="primary" sx={{ p: 1 }} onClick={handleClick}>
-                    <Icon icon={social.icon} width={16} height={16} />
-                  </IconButton>
-                  <Popover
-                    id={social.name}
-                    open={open}
-                    anchorEl={anchorEl}
-                    onClose={handleClose}
-                    anchorOrigin={{
-                      vertical: 'bottom',
-                      horizontal: 'left',
-                    }}
-                  >
-                    <Typography variant="caption">{social.link}</Typography>
-                  </Popover>
-                </>
-              ))}
-            </Stack>
+              <Stack
+                spacing={1.5}
+                direction="row"
+                justifyContent={{ xs: 'center', md: 'flex-start' }}
+                sx={{ mt: 5, mb: { xs: 5, md: 0 } }}
+              >
+                {SOCIALS.map((social) => (
+                  <>
+                    <IconButton key={social.name} color="primary" sx={{ p: 1 }} onClick={(e) => handleClick(e, social.name)}>
+                      <Icon icon={social.icon} width={16} height={16} />
+                    </IconButton>
+                    {title === social.name
+                      ? (
+                        <Popover
+                          id={social.name}
+                          open={open}
+                          anchorEl={anchorEl}
+                          onClose={handleClose}
+                          anchorOrigin={{
+                            vertical: 'bottom',
+                            horizontal: 'left',
+                          }}
+                        >
+                          <Typography variant="caption">{social.link}</Typography>
+                        </Popover>
+                      )
+                      : null}
+                  </>
+                ))}
+              </Stack>
+            </Grid>
+
+            <Grid item xs={12} md={7}>
+              <Stack spacing={5} direction={{ xs: 'column', md: 'row' }} justifyContent="space-between">
+                {LINKS.map((list) => {
+                  const { headline, children } = list;
+                  return (
+                    <Stack key={headline} spacing={2}>
+                      <Typography component="p" variant="overline">
+                        {headline}
+                      </Typography>
+                      {children.map((link) => (
+                        <Link
+                          to={link.href}
+                          key={link.name}
+                          color="inherit"
+                          variant="body2"
+                          component={RouterLink}
+                          sx={{ display: 'block' }}
+                        >
+                          {link.name}
+                        </Link>
+                      ))}
+                    </Stack>
+                  );
+                })}
+              </Stack>
+            </Grid>
           </Grid>
 
-          <Grid item xs={12} md={7}>
-            <Stack spacing={5} direction={{ xs: 'column', md: 'row' }} justifyContent="space-between">
-              {LINKS.map((list) => {
-                const { headline, children } = list;
-                return (
-                  <Stack key={headline} spacing={2}>
-                    <Typography component="p" variant="overline">
-                      {headline}
-                    </Typography>
-                    {children.map((link) => (
-                      <Link
-                        to={link.href}
-                        key={link.name}
-                        color="inherit"
-                        variant="body2"
-                        component={RouterLink}
-                        sx={{ display: 'block' }}
-                      >
-                        {link.name}
-                      </Link>
-                    ))}
-                  </Stack>
-                );
-              })}
-            </Stack>
-          </Grid>
-        </Grid>
-
-        <Typography
-          component="p"
-          variant="body2"
-          sx={{
-            mt: 10,
-            pb: 5,
-            fontSize: 13,
-            textAlign: { xs: 'center', md: 'left' }
-          }}
-        >
-          © 2021. All rights reserved
-        </Typography>
-      </Container>
-    </RootStyle>
+          <Typography
+            component="p"
+            variant="body2"
+            sx={{
+              mt: 10,
+              pb: 5,
+              fontSize: 13,
+              textAlign: { xs: 'center', md: 'left' }
+            }}
+          >
+            © 2021. All rights reserved
+          </Typography>
+        </Container>
+      </RootStyle>
+    </>
   );
 }
