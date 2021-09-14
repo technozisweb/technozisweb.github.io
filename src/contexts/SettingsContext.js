17d36c961190f790708fd20ecd6c189984723f30
Richa <richatechnozisx@gmail.com>
2021-09-14 21:58:55 +0530

    set theme color and browser tab icon

--- src/contexts/SettingsContext.js
+++ src/contexts/SettingsContext.js
@@ -13,16 +13,6 @@ const PRIMARY_COLOR = [
     name: 'default',
     ...palette.light.primary
   },
-  // PURPLE
-  {
-    name: 'purple',
-    lighter: '#EBD6FD',
-    light: '#B985F4',
-    main: '#7635dc',
-    dark: '#431A9E',
-    darker: '#200A69',
-    contrastText: '#fff'
-  },
   // CYAN
   {
     name: 'cyan',
@@ -33,36 +23,6 @@ const PRIMARY_COLOR = [
     darker: '#053D7A',
     contrastText: palette.light.grey[800]
   },
-  // BLUE
-  {
-    name: 'blue',
-    lighter: '#CCDFFF',
-    light: '#6697FF',
-    main: '#0045FF',
-    dark: '#0027B7',
-    darker: '#00137A',
-    contrastText: '#fff'
-  },
-  // ORANGE
-  {
-    name: 'orange',
-    lighter: '#FEF4D4',
-    light: '#FED680',
-    main: '#fda92d',
-    dark: '#B66816',
-    darker: '#793908',
-    contrastText: palette.light.grey[800]
-  },
-  // RED
-  {
-    name: 'red',
-    lighter: '#FFE3D5',
-    light: '#FFC1AC',
-    main: '#FF3030',
-    dark: '#B71833',
-    darker: '#7A0930',
-    contrastText: '#fff'
-  }
 ];
 
 SetColor.propTypes = {
@@ -72,28 +32,8 @@ SetColor.propTypes = {
 function SetColor(themeColor) {
   let color;
   const DEFAULT = PRIMARY_COLOR[0];
-  const PURPLE = PRIMARY_COLOR[1];
-  const CYAN = PRIMARY_COLOR[2];
-  const BLUE = PRIMARY_COLOR[3];
-  const ORANGE = PRIMARY_COLOR[4];
-  const RED = PRIMARY_COLOR[5];
 
   switch (themeColor) {
-    case 'purple':
-      color = PURPLE;
-      break;
-    case 'cyan':
-      color = CYAN;
-      break;
-    case 'blue':
-      color = BLUE;
-      break;
-    case 'orange':
-      color = ORANGE;
-      break;
-    case 'red':
-      color = RED;
-      break;
     default:
       color = DEFAULT;
   }
